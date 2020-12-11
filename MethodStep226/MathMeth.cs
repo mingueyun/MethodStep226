373109d8de293ee08558d0bc4fc12b2334829fd5
student <student@learncodinganywhere.com>
2020-12-10 17:27:52 -0800

    Add project files.

---
+++ MethodStep226/MathMeth.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodStep226
+{
+    public class MathMeth
+    {
+        public int math1(int a)
+        {
+            int math1 = a * a;
+            return math1;
+        }
+        public decimal math1(decimal b)
+        {
+            decimal math1 = b / 2;
+            return math1;
+        }
+        public string math1 (string c )
+        {
+            string math1 = c;
+            return math1;
+        }
+            
+        
+
+
+
+   
+
+
+    }
+
+}
---
+++ MethodStep226/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodStep226
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathMeth mathmeth = new MathMeth();
+            int one = 2;
+            int oneOne = mathmeth.math1(one);
+            Console.WriteLine(oneOne);
+
+            decimal two = 6;
+            decimal twoTwo = mathmeth.math1(two);
+            Console.WriteLine(twoTwo);
+            Console.ReadLine();
+
+            string three = "9";
+            string threeThree = mathmeth.math1(three);
+            int threees = Convert.ToInt32(threeThree);
+            Console.WriteLine(threees);
+
+            Console.ReadLine();
+
+        }
+    }
+}
